5742b7cca43c5828e08477cc3694763a5a8933fb
Younesmrc <younesarjdal@yahoo.com>
2024-01-29 21:41:16 +0100

    detail simu

--- Main Python/simulation.py
+++ Main Python/simulation.py
@@ -6,7 +6,7 @@ from Robot.robot import Robot
 pygame.init()
 
 # Définition des couleurs
-BLANC = (255, 255, 255)
+BLANC = (255,255,255)
 FPS = 30
 
 # Paramètres de la fenêtre
@@ -19,29 +19,29 @@ pygame.display.set_caption("Déplacement automatique du carré")
 
 # Définition du robot avec une image
 robot_image = pygame.image.load("1.png")
-robot_image = pygame.transform.scale(robot_image, (25, 25))  # Redimensionner l'image
-robot = Robot(50, 50, 25, 25, 1, 1)
+robot_image = pygame.transform.scale(robot_image,(25,25))  # Redimensionner l'image
+robot = Robot(50,50,25,25,1,1)
 
 # Définition de l'environnement
-environnement = Environnement(400, 400, fenetre)
+environnement = Environnement(400,400,fenetre)
 
 # Définition de l'action
-pas = 10
-i = 0
+pas=10
+i=0
 
 def action(robot):
     global i
     if i == 0:
-        if robot.avancer_vers(100, 100):
+        if robot.avancer_vers(100,100):
             i += 1
     if i == 1:
-        if robot.avancer_vers(150, 100):
+        if robot.avancer_vers(150,100):
             i += 1
     if i == 2:
-        if robot.avancer_vers(150, 150):
+        if robot.avancer_vers(150,150):
             i += 1
     if i == 3:
-        if robot.avancer_vers(100, 150):
+        if robot.avancer_vers(100,150):
             i = 0
 
 while True:
