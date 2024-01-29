790195b4706010c54a1a60c852f2e39e7de74b35
Younesmrc <younesarjdal@yahoo.com>
2024-01-29 20:56:32 +0100

    ajout v1 environnement avec juste la base

---
+++ Main Python/Environnement/environnement.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Environnement:
+
+    def __init__(self,largeur,hauteur,fenetre):
+        self.largeur=largeur
+        self.hauteur=hauteur
+        self.fenetre=fenetre
+    
+    
+    
+        
