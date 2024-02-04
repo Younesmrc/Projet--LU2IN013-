1185fe18d4c539fb17162c55e5ed3e094839cd1d
ficeloo <theophile.cros@free.fr>
2024-02-04 14:58:46 +0100

    Ajout de DocString dans les modules 'robot.py' et 'environnement.py'

--- Main_Python/Environnement/environnement.py
+++ Main_Python/Environnement/environnement.py
@@ -1,7 +1,11 @@
 import pygame
 
 class Environnement:
+    """Classe Environnement répertoriant le nécessaire pour créer un environnement
+        :param x: longueur de l'environnement
+        :param y: largeur de l'environnement
 
+    """
     def __init__(self,largeur,hauteur):
         self.largeur=largeur
         self.hauteur=hauteur
