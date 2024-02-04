f9c786d79e67f096976279fe56aae3300f9338be
Younesmrc <younesarjdal@yahoo.com>
2024-02-04 12:48:22 +0100

    retrait composant graphique dans environnement

--- Main_Python/Environnement/environnement.py
+++ Main_Python/Environnement/environnement.py
@@ -2,12 +2,9 @@ import pygame
 
 class Environnement:
 
-    def __init__(self,largeur,hauteur,fenetre):
+    def __init__(self,largeur,hauteur):
         self.largeur=largeur
         self.hauteur=hauteur
-        self.fenetre=fenetre
     
-    def rafraichir(self):
-        pygame.display.flip()
     
         
