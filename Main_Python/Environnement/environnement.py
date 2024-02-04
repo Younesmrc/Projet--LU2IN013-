601c37db3183df5f869d4c4cb1aa4c9c0658de88
Younesmrc <younesarjdal@yahoo.com>
2024-02-04 13:39:41 +0100

    ajout listeobject a l'environnement pour les collisions

--- Main_Python/Environnement/environnement.py
+++ Main_Python/Environnement/environnement.py
@@ -2,9 +2,10 @@ import pygame
 
 class Environnement:
 
-    def __init__(self,largeur,hauteur):
+    def __init__(self,largeur,hauteur,liste_object):
         self.largeur=largeur
         self.hauteur=hauteur
+        self.liste_object=object
     
     
         
