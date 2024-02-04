a8ad34a52473402ca84714577c3bb0fb6235bd71
Younesmrc <younesarjdal@yahoo.com>
2024-02-04 16:23:18 +0100

    erreur merge corrigé

--- Main_Python/Environnement/environnement.py
+++ Main_Python/Environnement/environnement.py
@@ -4,13 +4,8 @@ class Environnement:
     """Classe Environnement répertoriant le nécessaire pour créer un environnement
         :param x: longueur de l'environnement
         :param y: largeur de l'environnement
-
-<<<<<<< HEAD
     """
-    def __init__(self,largeur,hauteur):
-=======
     def __init__(self,largeur,hauteur,liste_object):
->>>>>>> 601c37db3183df5f869d4c4cb1aa4c9c0658de88
         self.largeur=largeur
         self.hauteur=hauteur
         self.liste_object=object
