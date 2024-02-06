c9d5eb4b8d41446785cbd14e1808ca4e06658486
GreyzRox <greyzrox@gmail.com>
2024-02-06 15:59:00 +0100

    unittest2

--- Main_Python/test/testrobot.py
+++ Main_Python/test/testrobot.py
@@ -1,15 +1,25 @@
 import unittest
 import sys
 sys.path.append("..")
-from model.robot import *
+from model.robot import Robot, Roue
+from model.environnement import Environnement
 
-class TestEnv
+class TestEnv(unittest.TestCase):
+    def test_instanceof(self):  # Correction du nom de la méthode
+        env = Environnement(400, 400, [])
+        self.assertIsInstance(env, Environnement)
 
 class TestRobot(unittest.TestCase):
-    def Robot_instanceof(self):
-        rob1 = Robot(0,0,5,6,200,200,)
-
+    def test_robot_instanceof(self):  # Correction du nom de la méthode
+        env1 = Environnement(400, 400, [])
+        rob1 = Robot(0, 0, 5, 6, 200, 200, env1,2)
+        self.assertIsInstance(rob1, Robot)
 
 class TestRoue(unittest.TestCase):
-    def Roue_instanceof(self):
-        r = Roue(2,)
+    def test_roue_instanceof(self):  # Correction du nom de la méthode
+        env2 = Environnement(400, 400, [])
+        rob2 = Robot(0, 0, 5, 6, 200, 200, env2,2)  # Correction de l'utilisation de env2
+        r = Roue(2, rob2, 45)
+
+if __name__ == '__main__':
+    unittest.main()
