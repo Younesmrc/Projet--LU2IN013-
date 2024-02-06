f9a3d0461462925c0d82ed1aef8b66e3fe39908f
GreyzRox <greyzrox@gmail.com>
2024-02-06 20:16:19 +0100

    unittest3 env

--- Main_Python/test/testrobot.py
+++ Main_Python/test/testrobot.py
@@ -10,12 +10,23 @@ class TestEnv(unittest.TestCase):
     def setUp(self):
         self.env = Environnement(400, 400, [])
         self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)
+        self.robotest = Robot(-10, -10, 10, 10, 200, 200, self.env, 2)
         self.roue = Roue(2, self.rob, 45)
 
     def test_instanceof(self):
+
         self.assertIsInstance(self.env, Environnement)
+
         self.env.ajoute_object(self.rob)
         self.assertIn(self.rob,self.env.liste_object)
+
+        self.env.ajoute_object(self.robotest)
+        self.env.controle_positions()
+        self.assertEqual(self.robotest.x,0)
+        self.assertEqual(self.robotest.y,0)
+
+        self.assertEqual(self.rob.x,self.robotest.x)
+        self.assertEqual(self.rob.y,self.robotest.y)
 
 
 class TestRobot(unittest.TestCase):
