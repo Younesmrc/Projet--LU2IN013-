1ecc5ce8ab7f699839919eebd732052b622a80dc
ould bouali <louizabouali2@gmail.com>
2024-02-07 09:02:33 +0100

    commentaire testrobot

--- Main_Python/tests/tests_robot.py
+++ Main_Python/tests/tests_robot.py
@@ -7,11 +7,13 @@ from model.environnement import Environnement
 class TestRobot(unittest.TestCase):
 
     def setUp(self):
-        self.env = Environnement(400, 400, [])#crée un environnement 
-        self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)
-        self.roue = Roue(2, self.rob)
+        #initialisation de l'environnement, du robot et de la roue pour chaque test
+        self.env = Environnement(400, 400, [])#crée un environnement de 400x400 sans obstacles
+        self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)#crée un robot 
+        self.roue = Roue(2, self.rob)#crée une roue
 
     def test_robot_instanceof(self):
+        #vérifie si les instance sont bien crées comme prévu 
         self.assertIsInstance(self.rob, Robot)
 
     def test_avancer(self):
@@ -37,6 +39,6 @@ class TestRoue(unittest.TestCase):
 
     def test_roue_instanceof(self):
         self.assertIsInstance(self.roue,Roue)
-
+#lancement des tests 
 if __name__ == '__main__':
     unittest.main()
