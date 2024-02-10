c9d03bf6a662a79ff8b53f68f19c443871e50311
Younesmrc <younesarjdal@yahoo.com>
2024-02-10 14:10:09 +0100

    methode creation fenetre

--- Main_Python/model/interface.py
+++ Main_Python/model/interface.py
@@ -2,3 +2,11 @@ import pygame
 from .environnement import Environnement
 from .robot import Robot
 
+BLANC = (255,255,255)
+FPS = 30
+ROUGE = (255, 0, 0)
+
+def creation_fenetre(environnement):
+    fenetre = pygame.display.set_mode((environnement.largeur,environnement.hauteur))
+    pygame.display.set_caption("Simulation robot")
+    return fenetre
