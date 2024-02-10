ab5b367dd10f37cce6479da31487353ee846e2e7
Younesmrc <younesarjdal@yahoo.com>
2024-02-10 14:10:44 +0100

    ajout de toute les methodes d'interface necessaire dans le fichier interface

--- Main_Python/model/interface.py
+++ Main_Python/model/interface.py
@@ -10,3 +10,24 @@ def creation_fenetre(environnement):
     fenetre = pygame.display.set_mode((environnement.largeur,environnement.hauteur))
     pygame.display.set_caption("Simulation robot")
     return fenetre
+
+def donner_image_robot(robot):
+    robot_image = pygame.image.load("robot3.png")
+    robot_image = pygame.transform.scale(robot_image,(robot.hauteur*2,robot.largeur*2))  # Redimensionner l'image
+    return robot_image
+
+def effacer_ecran(fenetre):
+    fenetre.fill(BLANC)
+
+def rotation(robot,robot_image):
+    rotated_robot = pygame.transform.rotate(robot_image,robot.get_angle())  # Utilisez l'angle du robot ici
+    return rotated_robot
+
+def dessine(robot,robot2,robot_image,fenetre):
+        rotated_robot = rotation(robot,robot_image)
+        rotated_rect = rotated_robot.get_rect(center=(round(robot.x), round(robot.y)))
+        pygame.draw.rect(fenetre,ROUGE,(round(robot2.x), round(robot2.y),20,20))
+        fenetre.blit(rotated_robot, rotated_rect.topleft)
+
+def rafraichissement():
+    return pygame.display.flip()
