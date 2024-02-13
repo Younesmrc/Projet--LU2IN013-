de26468b4dff9514d5a0196e22a87e3b1b93ee0f
GreyzRox <greyzrox@gmail.com>
2024-02-13 20:05:22 +0100

    maj robotest

--- Main_Python/tests/tests_robot.py
+++ Main_Python/tests/tests_robot.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 sys.path.append("..")
-from model.robot import Robot, Roue
+from model.robot import Robot
 from model.environnement import Environnement
 
 class TestRobot(unittest.TestCase):
@@ -9,36 +9,17 @@ class TestRobot(unittest.TestCase):
     def setUp(self):
         #initialisation de l'environnement, du robot et de la roue pour chaque test
         self.env = Environnement(400, 400, [])#crée un environnement de 400x400 sans obstacles
-        self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)#crée un robot 
-        self.roue = Roue(2, self.rob)#crée une roue
+        self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)#crée un robot
 
-    def test_robot_instanceof(self):
-        #vérifie si les instance sont bien crées comme prévu 
-        self.assertIsInstance(self.rob, Robot)
+    def test_angle(self):
+        # Créez une instance de votre classe Robot avec des coordonnées directionnelles appropriées
+        self.rob.direction_x= 1
+        self.rob.direction_y = 1
+        # Appelez la méthode get_angle()
+        angle_resultat = self.rob.get_angle()
+        # Vérifiez que l'angle est proche de 0 degré (vers la droite)
+        print(self.assertAlmostEqual(angle_resultat, 45, delta=0.01))
 
-    def test_avancer(self):
-        robx = self.rob.x
-        roby = self.rob.y
-        self.rob.avancer(4)
-        self.assertNotEqual(robx,self.rob.x)
-        self.assertNotEqual(roby,self.rob.y)
-
-    def test_reculer(self):
-        robx = self.rob.x
-        roby = self.rob.y
-        self.rob.reculer(6)
-        self.assertNotEqual(robx,self.rob.x)
-        self.assertNotEqual(roby,self.rob.y)
-
-class TestRoue(unittest.TestCase):
-
-    def setUp(self):
-        self.env = Environnement(400, 400, [])
-        self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)
-        self.roue = Roue(2, self.rob)
-
-    def test_roue_instanceof(self):
-        self.assertIsInstance(self.roue,Roue)
 #lancement des tests 
 if __name__ == '__main__':
     unittest.main()
