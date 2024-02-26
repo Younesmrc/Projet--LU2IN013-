2a28fcbc6f423d82fa09c270a0b20fb13c92d02e
lisamoula <lisa.moula@outlook.FR>
2024-02-26 17:24:53 +0100

    correction classe controleur_D

--- Main_Python/model/strategie/contoleurD.py
+++ Main_Python/model/strategie/contoleurD.py
@@ -3,11 +3,17 @@ from ..environnement import Environnement
 from .tourner_D import Tourner_D
 
 
-class ControleurD:
+class controleurD:
 
     def __init__(self,robot,environnement,angle):
-        angle=angle
-        self.strats = [Tourner_D(robot,environnement,angle)]
-        self.cur = -1
+        self.tourner_d = Tourner_D(robot, environnement, angle)
+        
 
-    
+    def start(self):
+        self.tourner_D.start()
+
+    def step(self):
+        self.tourner_D.step()
+
+    def stop(self):
+        return self.tourner_.stop()
