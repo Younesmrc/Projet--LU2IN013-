0c329523185885917eeec69d18d38efc21fe2bc1
lisamoula <lisa.moula@outlook.FR>
2024-02-26 17:33:17 +0100

    correction classe controleur_G

--- Main_Python/model/strategie/contoleur_G.py
+++ Main_Python/model/strategie/contoleur_G.py
@@ -6,14 +6,14 @@ from .tourner_G import Tourner_G
 class controleur_G:
 
     def __init__(self,robot,environnement,angle):
-        self.tourner_d = Tourner_G(robot, environnement, angle)
+        self.tourner_G = Tourner_G(robot, environnement, angle)
         
 
     def start(self):
-        self.tourner_D.start()
+        self.tourner_G.start()
 
     def step(self):
-        self.tourner_D.step()
+        self.tourner_G.step()
 
     def stop(self):
-        return self.tourner_.stop()
+        return self.tourner_G.stop()
