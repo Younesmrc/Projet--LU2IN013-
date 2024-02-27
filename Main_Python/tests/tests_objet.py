2453889501276b8bdc55735b913813b9723d6045
ficeloo <theophile.cros@free.fr>
2024-02-27 20:42:50 +0100

    Créations tests unitaires pour la classe 'Objet.py'

---
+++ Main_Python/tests/tests_objet.py
@@ -0,0 +1,21 @@
+import unittest
+
+from model.objet import Objet
+
+class TestObjet(unittest.TestCase):
+
+    def setUp(self):
+
+        # Initialisation d'objet 1 et 2 de paramètres différents
+        self.objet_1 = Objet(0,0,10,10) # Objet aux coordonnées (0,0)
+        self.objet_2 = Objet(10,20,10,10) # Objet aux coordonnées (10,20)
+
+    def test_est_dans_obstacle(self):
+
+        # Tests les coordonnées de l'objet 1 dans différentes configurations
+        self.assertTrue(self.objet_1.est_dans_obstacle(7,8))
+        self.assertFalse(self.objet_1.est_dans_obstacle(11,25))
+
+        # Tests les coordonnées de l'objet 1 dans différentes configurations
+        self.assertTrue(self.objet_2.est_dans_obstacle(15,25))
+        self.assertFalse(self.objet_2.est_dans_obstacle(9,17))
