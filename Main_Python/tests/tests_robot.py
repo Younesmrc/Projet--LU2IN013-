5ced7822d483b2bf0d88fd7f5f34af4d3f6d58c2
GreyzRox <guillaume.dupart.pro@gmail.com>
2024-02-27 22:25:02 +0100

    ajout de tests_detection_obstacle

--- Main_Python/tests/tests_robot.py
+++ Main_Python/tests/tests_robot.py
@@ -11,6 +11,8 @@ class TestRobot(unittest.TestCase):
         #initialisation de l'environnement, du robot et de la roue pour chaque test
         self.env = Environnement(400, 400, [])#crée un environnement de 400x400 sans obstacles
         self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)#crée un robot
+        self.obj = Objet(0,0,3,3)
+        self.env.ajoute_object(self.obj)
 
     #Faire update position
         
@@ -37,10 +39,29 @@ class TestRobot(unittest.TestCase):
         self.assertEqual(self.rob.vitesse_droite,2)
 
         
-    #faire detection_obstacle
-        #Note pour moi meme, l'objectif est de voir si le robot detecte un obstacle dans la liste, il faut que ca soit false
-        #Puis ensuite faire changer la direction du robot et il faut que cette fois il detecte l'obstacle
-        #Facultatif, ca serait cool de tester la distance entre le robot et l'obstacle
+
+    def test_detection_obstacle(self):
+        self.obj.x = 100
+        self.obj.y = 0
+        #Nous placons un objet en coordonnées 100 0
+        self.rob.direction_x = 0
+        self.rob.direction_y = -1
+        #Le robot regarde en bas, soit dans une direction ou il n'y a pas d'objet
+        #Verifions que le robot ne detecte aucun objet
+        self.assertEqual(self.rob.detection_obstacle(self.env.liste_object),None)
+        self.rob.direction_x = 1
+        self.rob.direction_y = 0
+        #A présent, le robot regarde en direction de l'obstacle
+        self.assertNotEqual(self.rob.detection_obstacle(self.env.liste_object),None)
+        #Regardons a présent si la distance entre le robot et l'obstacle est bien de 100
+        self.assertEqual(self.rob.detection_obstacle(self.env.liste_object),100)
+        #La distance est bien de 100, faisons avancer le robot pour voir si cela marche toujours
+        self.rob.x = 5
+        #Avec ces parametres, le robot n'est plus qu'a une distance de 95 de l'obstacle, verifions cela
+        self.assertEqual(self.rob.detection_obstacle(self.env.liste_object),95)
+
+
+
 
 #lancement des tests 
 if __name__ == '__main__':
