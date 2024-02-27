de0819352aee80476a19948a0cf9126126f80919
GreyzRox <guillaume.dupart.pro@gmail.com>
2024-02-27 16:58:59 +0100

    ajout des objectifs

--- Main_Python/tests/tests_robot.py
+++ Main_Python/tests/tests_robot.py
@@ -12,6 +12,8 @@ class TestRobot(unittest.TestCase):
         self.env = Environnement(400, 400, [])#crée un environnement de 400x400 sans obstacles
         self.rob = Robot(0, 0, 5, 6, 200, 200, self.env,2)#crée un robot
 
+    #Faire update position
+        
     def test_angle(self):
         """L'objectif dans ce test est de donner une direction de base, puis de verifier l'angle de direction du robot en degrés, avec une marge d'erreur delta"""
         self.rob.direction_x= 1
@@ -24,6 +26,10 @@ class TestRobot(unittest.TestCase):
         # l'angle du robot doit etre maintenant = a 0 (tout a droite)
         self.assertNotEqual(self.rob.get_angle(),45)
         self.assertEqual(self.rob.get_angle(),0)
+
+    #faire set_vitesse
+        
+    #faire detection_obstacle
 
 #lancement des tests 
 if __name__ == '__main__':
