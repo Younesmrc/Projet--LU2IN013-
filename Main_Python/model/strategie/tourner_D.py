7cb5f8c3c071b0ea8bb2a6fd79375605246af23c
ficeloo <theophile.cros@free.fr>
2024-03-01 19:38:07 +0100

    Ajout de la DocString + Commentaire

--- Main_Python/model/strategie/tourner_D.py
+++ Main_Python/model/strategie/tourner_D.py
@@ -8,7 +8,8 @@ class Tourner_D:
         robot (Robot): L'objet robot à contrôler.
         environnement: L'environnement dans lequel le robot opère.
         angle (float): L'angle de rotation à effectuer en degrés.
-        
+        cur (int): Compteur courant du nombre de step effectué
+        angle_vise(float): Angle voulu par le robot par rapport à son angle de départ
 
     Méthodes:
         start(): Initialise l'angle parcouru par le robot.
@@ -26,17 +27,19 @@ class Tourner_D:
         
     def start(self):
         """ Initialise l'angle parcouru par le robot."""
-        self.cur = 0
+        self.cur = 0 # Initialisation du compteur à 0
         self.robot.set_vitesse(-1, 1)  # Rotation vers la droite
-        self.angle_vise = self.robot.get_angle() + self.angle
+        self.angle_vise = (self.robot.get_angle() + self.angle) % 360 # Calcul de l'angle final
         print("Angle visé au début de la rotation ",self.angle_vise)
 
     def step(self):
         """ Effectue un petit pas de rotation vers la droite."""
         
         angle_actuel = self.robot.get_angle()
+        # Calcul de l'angle restant par rapport à l'angle réel du robot
         angle_restant = self.angle_vise - self.robot.get_angle()
         
+        # Calcul de la vitesse angulaire en fonction du nombre de step
         if self.cur != 0:
             vitesse_angulaire = (self.angle - angle_restant) / self.cur
         else:
@@ -46,7 +49,8 @@ class Tourner_D:
         if vitesse_angulaire > angle_restant :
             print("ce cas arrive")
             self.robot.set_vitesse(-0.05, 0.05)
-       
+
+        # Augmentation du compteur
         self.cur += 1
         
     def stop(self):
