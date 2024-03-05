025acc481af38ce920eb21e2b97da882cc41971f
lisamoula <lisa.moula@outlook.FR>
2024-03-05 17:00:15 +0100

    ajout de la classe mockup

---
+++ Main_Python/irl/mockup.py
@@ -0,0 +1,41 @@
+import math
+
+
+class Robot2I013Mockup(object):
+    """
+        Une image de l'api robot pour tester si on a une parfaite utilisation des fonctions
+    """
+
+    WHEEL_BASE_WIDTH = 100
+    WHEEL_DIAMETER = 50
+
+    def __init__(self):
+        self.MOTOR_LEFT = 0
+        self.MOTOR_RIGHT = 0
+
+    def set_led(self, led, red, green, blue):
+        pass
+
+    def get_voltage(self):
+        pass
+
+    def set_motor_dps(self, port, dps):
+        pass
+
+    def get_motor_position(self):
+        return (0, 0)
+
+    def offset_motor_encoder(self, port, offset):
+        pass
+
+    def get_distance(self):
+        return 10
+
+    def servo_rotate(self, position):
+        pass
+
+    def stop(self):
+        pass
+
+    def get_image(self):
+        pass
