e0369cfdda899c4d0bf79d15c3eec6f6cf92598c
louisabouali <louizabouali2@gmail.com>
2024-03-06 10:23:58 +0100

    modification de faire_rond

--- Main_Python/model/strategie/faire_rond.py
+++ Main_Python/model/strategie/faire_rond.py
@@ -21,30 +21,30 @@ class TracerRond:
 
     """
     
-    def __init__(self, robot, environnement, rayon, vitesse_lineaire, vitesse_angulaire):
+    def __init__(self, robot, environnement, distance):
         self.robot = robot
         self.environnement = environnement
-        self.rayon = rayon
-        self.vitesse_lineaire = vitesse_lineaire
-        self.vitesse_angulaire = vitesse_angulaire
+        self.distance = distance 
+
         self.angle_vise = 0
 
     def start(self):
         """ Initialise les paramètres nécessaires pour tracer le rond."""
-        self.angle_vise = 0
+        self.parcouru = 0
+        self.robot.set_vitesse(2, 1) 
 
     def step(self):
-        """ Effectue un petit pas de rotation pour faire tracer le rond."""
-        temps_passe = self.robot.update_temps_passe()
-        self.angle_vise +=self.vitesse_lineaire * temps_passe 
-        distance_parcourue = self.vitesse_angulaire * temps_passe
+        """Déplace le robot vers l'avant d'un petit pas."""
+                
 
-         # Calcul des vitesses des roues pour un mouvement circulaire
-        vitesse_gauche = self.vitesse_lineaire - (self.vitesse_angulaire * self.rayon)
-        vitesse_droite = self.vitesse_lineaire + (self.vitesse_angulaire * self.rayon)
-
-        self.robot.set_vitesse(vitesse_gauche, vitesse_droite)
+        # Calcul la distance parcouru en fonction de la vitesse
+        self.parcouru += (self.robot.vitesse_gauche + self.robot.vitesse_droite) / 2  
+        
+        if self.stop():
+            return
+        
+       
 
     def stop(self):
-        """ Vérifie si l'angle voulu est atteint."""
-        return abs(self.robot.get_angle() - self.angle_vise) < 0.1
+        """Vérifie si le robot a parcouru la distance spécifiée."""
+        return self.parcouru > self.distance * 2.85
