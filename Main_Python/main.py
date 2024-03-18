295a980abeb5e9c49072bf12aab619b2af67e2e3
ficeloo <theophile.cros@free.fr>
2024-03-18 16:34:41 +0100

    Ajout d'un sélectionneur de version du robot

--- Main_Python/main.py
+++ Main_Python/main.py
@@ -4,28 +4,40 @@ import pygame
 
 FPS = 60
 
-graphique = False #true pour ouvrir la fenetre false sinon
+# On détermine quel robot on utilise (1 => robot simulé / 2 => robot réel)
+robot_version = 1 
 
-#vitesse des updates du robot :
+if robot_version == 1:
+    graphique = True # True pour ouvrir la fenetre false sinon
+
+elif robot_version == 2:
+    graphique = False # True pour ouvrir la fenetre false sinon
+
+else:
+    print("Version de robot non prise en charge")
+
+
+# Vitesse des updates du robot :
 deltat=2
 
-#taille de l'environnement
+# Taille de l'environnement
 largeur_environnement = 500
 hauteur_environnement = 500
-#taille de la fenetre de simulation
+
+# Taille de la fenetre de simulation
 largeur_simu = 500
 hauteur_simu = 500
 
-#Definition variables du robot
+# Definition variables du robot
 
 robot_x,robot_y = (150,150)  
 robot_longueur,robot_largeur = (30,30) 
 direction_x,direction_y = (1,0)
 
-#variable pour la stratégie rond 
+# Variable pour la stratégie rond 
 rayon = 10
 vitesse_lineaire = 1
 vitesse_angulaire = math.radians(5)
 
 
-run_simulation(FPS,graphique,largeur_environnement,hauteur_environnement,largeur_simu,hauteur_simu,robot_x,robot_y,robot_longueur,robot_largeur,direction_x,direction_y,rayon, vitesse_lineaire,vitesse_angulaire,deltat)
+run_simulation(FPS,graphique,largeur_environnement,hauteur_environnement,largeur_simu,hauteur_simu,robot_x,robot_y,robot_longueur,robot_largeur,direction_x,direction_y,rayon, vitesse_lineaire,vitesse_angulaire,deltat,robot_version)
