b24b523c5505354483c7edf3f76074b7256b1e70
Younesmrc <younesarjdal@yahoo.com>
2024-03-19 22:06:33 +0100

    correction appel motor left et right

--- Main_Python/model/irl/robotadaptateur.py
+++ Main_Python/model/irl/robotadaptateur.py
@@ -24,8 +24,8 @@ class RobotAdaptateur:
         self.positions_precedentes = []
 
     def set_vitesse(self,vitesse_gauche,vitesse_droite):
-        self.robot_mockup.set_motor_dps(self.robot_mockup._gpg.MOTOR_LEFT,vitesse_gauche)
-        self.robot_mockup.set_motor_dps(self.robot_mockup._gpg.MOTOR_RIGHT,vitesse_droite)
+        self.robot_mockup.set_motor_dps(self.robot_mockup.MOTOR_LEFT,vitesse_gauche)
+        self.robot_mockup.set_motor_dps(self.robot_mockup.MOTOR_RIGHT,vitesse_droite)
         self.vitesse_gauche=vitesse_gauche
         self.vitesse_droit=vitesse_droite
 
