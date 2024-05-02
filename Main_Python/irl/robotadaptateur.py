e89244da896c600025552417a114cdffb5b9319b
Younesmrc <younesarjdal@yahoo.com>
2024-05-02 13:16:28 +0200

    ajout nouveau calcule pour la rotation du robot a tester

--- Main_Python/irl/robotadaptateur.py
+++ Main_Python/irl/robotadaptateur.py
@@ -46,23 +46,32 @@ class RobotAdaptateur:
             vitesse_droite (float): Vitesse de la roue droite.
         """
 
-        # Calcul la distance linéaire parcouru par une seule roue (droite et gauche)
-        print("angle gauche :" + str(self.robot.get_motor_position()[0] / 360)+"angle droit :"+ str(self.robot.get_motor_position()[1] / 360))
-        distance_lineaire_parcouru_rd = self.robot.WHEEL_CIRCUMFERENCE * (self.robot.get_motor_position()[0] / 360)
-        distance_lineaire_parcouru_rg = self.robot.WHEEL_CIRCUMFERENCE * (self.robot.get_motor_position()[1] / 360)
-        
-        
+        angle_gauche,angle_droit = self.robot.get_motor_position()
+        #PRINT ANGLE
+        print("angle gauche :" + str(angle_gauche)+"angle droit :"+ str(angle_droit))
+
+        #CALCULE DISTANCE
+        #distance_lineaire_parcouru_rd = self.rayon_roue * math.radians(self.robot.get_motor_position()[0])
+        #distance_lineaire_parcouru_rg = self.rayon_roue * math.radians(self.robot.get_motor_position()[1])
+        distance_lineaire_parcouru_rd = self.robot.WHEEL_CIRCUMFERENCE * (angle_gauche / 360)
+        distance_lineaire_parcouru_rg = self.robot.WHEEL_CIRCUMFERENCE * (angle_droit/ 360)
+
+        #PRINT DISTANCE 
+        print("distance gauche "+str(distance_lineaire_parcouru_rg)+" distance droit : "+ str(distance_lineaire_parcouru_rd))
+
+
         distance_totale_parcourue = (distance_lineaire_parcouru_rd + distance_lineaire_parcouru_rg) / 2.0
         self.distance_parcouru+=distance_totale_parcourue
 
-
-        #MISE A JOUR DES COORDONNES 
-
-        #rotation
+        #ROTATION
+        
+        #rotation = self.calcul_rotation(angle_gauche,angle_droit)
+        #self.angle_parcouru+=rotation
         rotation = (distance_lineaire_parcouru_rd - distance_lineaire_parcouru_rg) / self.largeur
-        print("distance gauche "+str(distance_lineaire_parcouru_rg)+" distance droit : "+ str(distance_lineaire_parcouru_rd))
         self.angle_parcouru += (rotation*180/math.pi)
         
+        #misa a jour direction position... ---------------------------------------------------------------------------------
+
         # Mise à jour de la direction du robot
         nouvelle_direction_x = self.direction_x * math.cos(rotation) - self.direction_y * math.sin(rotation)
         nouvelle_direction_y = self.direction_x * math.sin(rotation) + self.direction_y * math.cos(rotation)
@@ -128,3 +137,32 @@ class RobotAdaptateur:
 
     def condition_angle(self,angle_vise):
         return self.angle_parcouru <= angle_vise
+    
+    def calcul_rotation(self,angle_gauche, angle_droite):
+        # Convertir les angles en radians
+        angle_gauche_rad = math.radians(angle_gauche)
+        angle_droite_rad = math.radians(angle_droite)
+        
+        # Calculer les distances parcourues par chaque roue
+        distance_gauche = angle_gauche_rad * self.rayon_roue
+        distance_droite = angle_droite_rad * self.rayon_roue
+        
+        # Calculer la différence de distance
+        difference_distance = distance_droite - distance_gauche
+        
+        # Calculer l'angle de rotation en radians
+        angle_rotation_rad = difference_distance / self.rayon_roue
+        
+        # Retourner l'angle de rotation en degrés
+        return math.degrees(angle_rotation_rad)
+    
+    def calcul_nouveau_vecteur(x, y, angle_rotation):
+        # Conversion de l'angle de rotation en radians
+        angle_rotation_rad = math.radians(angle_rotation)
+        
+        # Calcul des nouveaux composants x et y du vecteur
+        nouveau_x = x * math.cos(angle_rotation_rad) - y * math.sin(angle_rotation_rad)
+        nouveau_y = x * math.sin(angle_rotation_rad) + y * math.cos(angle_rotation_rad)
+        
+        # Retourne les nouveaux composants x et y
+        return nouveau_x, nouveau_y
