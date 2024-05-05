1d69805a8f75309380116f6b555b36255ca453bd
lisamoula <lisa.moula@outlook.FR>
2024-05-05 19:18:10 +0200

    add  de la méthode qui capture les images (pas encore testé)

---
+++ Main_Python/screen.py
@@ -0,0 +1,15 @@
+from irl.RobotReel import Robot2I013 as R
+from PIL import Image
+from time import sleep
+import numpy as np
+
+r = R()
+
+r.servo_rotate(90) #mettre la cam au milieu
+sleep(1)
+
+img = r.get_image()
+img = np.array(img)
+
+img = Image.fromarray(img)
+img.save("image.png")
